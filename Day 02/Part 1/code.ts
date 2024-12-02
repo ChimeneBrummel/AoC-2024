f93f638a64d91dd978deb7f5db98e4e6eb05e766
Chimène Brummel <chimeneb@delta-n.nl>
2024-12-02 10:54:05 +0100

    Part 1 of Day 2

--- Day 02/Part 1/code.ts
+++ Day 02/Part 1/code.ts
@@ -1,2 +1,17 @@
-const example = await Deno.readTextFile("example.txt");
-console.log(example)
+const input = await Deno.readTextFile("input.txt");
+const lines = input.split('\n');
+
+const isMonotonic = (report: number[]) => {
+    const isIncreasing = report.every((num, i) => i === 0 || num > report[i - 1]);
+    const isDecreasing = report.every((num, i) => i === 0 || num < report[i - 1]);
+    return isIncreasing || isDecreasing;
+};
+
+const hasValidDifferences = (report: number[]) =>
+    report.every((num, i, arr) => i === 0 || Math.abs(num - arr[i - 1]) >= 1 && Math.abs(num - arr[i - 1]) <= 3);
+
+const safeReports = lines
+    .map(line => line.split(' ').map(Number))
+    .filter(report => isMonotonic(report) && hasValidDifferences(report));
+
+console.log(safeReports.length);
