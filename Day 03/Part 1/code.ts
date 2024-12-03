588ef2be610bdd3ab0f74ed284be00ed7463dc3c
ChimeneBrummel <20137729@student.hhs.nl>
2024-12-03 15:17:28 +0100

    Solved Day 3 part 1, part 2 in progress

--- Day 03/Part 1/code.ts
+++ Day 03/Part 1/code.ts
@@ -1,2 +1,8 @@
-const example = await Deno.readTextFile("example.txt");
-console.log(example)
+const input = await Deno.readTextFile("input.txt");
+
+const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const matches = [...input.matchAll(regex)];
+
+const totalSum = matches.reduce((sum, match) => sum + parseInt(match[1]) * parseInt(match[2]), 0);
+
+console.log(totalSum);
--- Day 03/Part 2/code.ts
+++ Day 03/Part 2/code.ts
@@ -1,2 +1,35 @@
-const example = await Deno.readTextFile("example.txt");
-console.log(example)
+const input = await Deno.readTextFile("input.txt");
+
+const mulRegex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const doRegex = /do\(\)/g;
+const dontRegex = /don't\(\)/g;
+
+// Added .sort to ensure the regex matches are processed in the correct order. Otherwise mulEnabled kept defaulting to true.
+//Leading to incorrect answers. This happened due to the array not having been sorted based on index of the input.txt.
+const array = [
+    ...input.matchAll(mulRegex),
+    ...input.matchAll(doRegex),
+    ...input.matchAll(dontRegex)
+].sort((a, b) => a.index - b.index);
+
+let mulEnabled = true
+
+const totalSum = array.reduce(
+    (sum, match) => {
+        if (mulRegex.test(match[0]) && mulEnabled) {
+            const [_fullMatch, firstNumber, secondNumber] = match;
+            sum += parseInt(firstNumber) * parseInt(secondNumber);
+        }
+
+        else if (doRegex.test(match[0])) {
+            mulEnabled = true;
+        }
+
+        else if (dontRegex.test(match[0])) {
+            mulEnabled = false;
+        }
+
+        return sum;
+    }, 0)
+
+console.log(totalSum);
