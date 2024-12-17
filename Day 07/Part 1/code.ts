6a615bc459871ada4fee583dfbcfe30f9afd86ee
ChimeneBrummel <20137729@student.hhs.nl>
2024-12-17 16:17:53 +0100

    Solved day 7

--- Day 07/Part 1/code.ts
+++ Day 07/Part 1/code.ts
@@ -1,2 +1,32 @@
-const example = await Deno.readTextFile("example.txt");
-console.log(example)
+const input = await Deno.readTextFile("input.txt");
+
+const lines: [number, number[]][] = input.split('\n').map(line => {
+    const [testValue, numbers] = line.split(':');
+    return [parseInt(testValue), numbers.trim().split(' ').map(Number)];
+});
+
+const generateEquations = (numbers: number[], index: number = 0, current: string = ''): string[] =>
+    index === numbers.length - 1
+        ? [current + numbers[index]]
+        : ['+', '*'].flatMap(operator => generateEquations(numbers, index + 1, current + numbers[index] + operator));
+
+
+const evaluateEquation = (equation: string): number =>
+    equation.split(/(\+|\*)/).reduce((result, token, index, tokens) =>
+        index % 2 === 0
+            ? result
+            : token === '+'
+                ? result + parseInt(tokens[index + 1])
+                : result * parseInt(tokens[index + 1]),
+        parseInt(equation.split(/(\+|\*)/)[0])
+    );
+
+const isValidEquation = (testValue: number, numbers: number[]): boolean =>
+    generateEquations(numbers).some(equation => evaluateEquation(equation) === testValue);
+
+const totalCalibrationResult = lines
+    .filter(([testValue, numbers]) => isValidEquation(testValue, numbers))
+    .map(([testValue]) => testValue)
+    .reduce((sum, value) => sum + value, 0);
+
+console.log(totalCalibrationResult);
